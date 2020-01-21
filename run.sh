9cd2354c2e999e49fa1d85670475d52fed0e6597
kevinzeladacl <yo@kevinzelada.cl>
2020-01-20 22:51:57 -0300

    update some files and update master script

--- run.sh
+++ run.sh
@@ -2,66 +2,67 @@
 clear
 cat << "EOF"
 
-              _                            _              _             _          _____ 
-             | |                          | |            | |           (_)        / __  \
+							_                            _              _             _          _____ 
+						 | |                          | |            | |           (_)        / __  \
 __      _____| | ___ ___  _ __ ___   ___  | |_ ___     __| | __ _ _ __  _  __   __`' / /'
 \ \ /\ / / _ \ |/ __/ _ \| '_ ` _ \ / _ \ | __/ _ \   / _` |/ _` | '_ \| | \ \ / /  / /  
  \ V  V /  __/ | (_| (_) | | | | | |  __/ | || (_) | | (_| | (_| | |_) | |  \ V / ./ /___
-  \_/\_/ \___|_|\___\___/|_| |_| |_|\___|  \__\___/   \__,_|\__,_| .__/|_|   \_/  \_____/
-                                                                 | |                     
-                                                                 |_|     for appactory.cl
-                                                                         by  @kevinzeladacl   
+	\_/\_/ \___|_|\___\___/|_| |_| |_|\___|  \__\___/   \__,_|\__,_| .__/|_|   \_/  \_____/
+																																 | |                     
+																																				 by  @kevinzeladacl   
 
 ==========================================================================================
 
 SELECT A OPTION (enter number):
 
-
+============LOCAL/DEV OPTIONS ==========
 0 - RUN WITH DEBUG CONSOLE (DEV)
-1 - RUN WITH DEBUG CONSOLE (PROD)
-2 - AUTO LOCAL MIGRATIONS
-3 - AUTO PROD MIGRATIONS
-4 - CREATE SUPER USER
+1 - AUTO LOCAL MIGRATIONS
+2 - CREATE SUPER USER LOCAL
+
+
+============PROD OPTIONS ==========
+51 - RUN WITH DEBUG CONSOLE (PROD)
+52 - AUTO PROD MIGRATIONS
 999 - INSTALL PROJECT LOCAL
 
 ===========================================================================================
 
 EOF
 
-read opcion
-case $opcion in
-  999) 
-     echo "Instando ..."
-     pip install -r requirements/initial.txt
-     python3 manage.py makemigrations --settings=dapi.settings.local
-     python3 manage.py migrate --settings=dapi.settings.local
-  ;;
-  0) 
-	 echo "RUN WITH DEBUG CONSOLE (DEV)..."
-     python3 manage.py runserver 0.0.0.0:8000 --settings=dapi.settings.local
-  ;;
-  1) 
-   echo "RUN WITH DEBUG CONSOLE (PROD)..."
-     python3 manage.py runserver 0.0.0.0:8000 --settings=dapi.settings.prod
-  ;;
-  2)
-     echo "RUN AUTO LOCAL MIGRATIONS..."
-     pip install -r requirements/initial.txt
-     python3 manage.py makemigrations --settings=dapi.settings.local
-     python3 manage.py migrate --settings=dapi.settings.local
-  ;;
-  3)
-     echo "RUN AUTO PROD MIGRATIONS..."
-     pip install -r requirements/initial.txt
-     python3 manage.py makemigrations --settings=dapi.settings.prod
-     python3 manage.py migrate --settings=dapi.settings.prod
-  ;;
-  4) 
-   echo "CREATE SUPER USER..."
-     python3 manage.py createsuperuser --settings=dapi.settings.local
-  ;;
-  *)
-     echo "Select a valid option 77"
-  ;;
+read option
+case $option in
+	999) 
+		 echo "Installing now ..."
+		 pip install -r requirements.txt
+		 python3 manage.py makemigrations --settings=dapi.settings.local
+		 python3 manage.py migrate --settings=dapi.settings.local
+	;;
+	0) 
+		 echo "RUN WITH DEBUG CONSOLE (DEV)..."
+		 python3 manage.py runserver 0.0.0.0:8000 --settings=dapi.settings.local
+	;;
+	2)
+		 echo "RUN AUTO LOCAL MIGRATIONS..."
+		 pip install -r requirements.txt
+		 python3 manage.py makemigrations --settings=dapi.settings.local
+		 python3 manage.py migrate --settings=dapi.settings.local
+	;;
+	3) 
+		 echo "CREATE SUPER USER..."
+		 python3 manage.py createsuperuser --settings=dapi.settings.local
+	;;
+	51) 
+		 echo "RUN WITH DEBUG CONSOLE (PROD)..."
+		 python3 manage.py runserver 0.0.0.0:8000 --settings=dapi.settings.prod
+	;;
+	52)
+		 echo "RUN AUTO PROD MIGRATIONS..."
+		 python3 manage.py makemigrations --settings=dapi.settings.prod
+		 python3 manage.py migrate --settings=dapi.settings.prod
+	;;
+	*)
+		 echo "Select a valid option 77"
+	;;
 
-esac
+esacs
