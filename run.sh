bfd08dc104bab362603d16d31b95992367bd758d
Kevinzeladacl <iam@kevinzelada.cl>
2020-06-03 22:39:45 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,14 +2,17 @@
 clear
 cat << "EOF"
 
-							_                            _              _             _          _____ 
-						 | |                          | |            | |           (_)        / __  \
+              _                            _              _             _          _____ 
+             | |                          | |            | |           (_)        / __  \
 __      _____| | ___ ___  _ __ ___   ___  | |_ ___     __| | __ _ _ __  _  __   __`' / /'
 \ \ /\ / / _ \ |/ __/ _ \| '_ ` _ \ / _ \ | __/ _ \   / _` |/ _` | '_ \| | \ \ / /  / /  
  \ V  V /  __/ | (_| (_) | | | | | |  __/ | || (_) | | (_| | (_| | |_) | |  \ V / ./ /___
-	\_/\_/ \___|_|\___\___/|_| |_| |_|\___|  \__\___/   \__,_|\__,_| .__/|_|   \_/  \_____/
-																																 | |                     
-																																				 by  @kevinzeladacl   
+  \_/\_/ \___|_|\___\___/|_| |_| |_|\___|  \__\___/   \__,_|\__,_| .__/|_|   \_/  \_____/
+                                                                 | |                     
+                                                                 |_|     by  @kevinzeladacl
+                                                                         
+
+PROJECT: <your_project_name>
 
 ==========================================================================================
 
@@ -32,37 +35,39 @@ EOF
 
 read option
 case $option in
-	999) 
-		 echo "Installing now ..."
-		 pip install -r requirements.txt
-		 python3 manage.py makemigrations --settings=dapi.settings.local
-		 python3 manage.py migrate --settings=dapi.settings.local
-	;;
-	0) 
-		 echo "RUN WITH DEBUG CONSOLE (DEV)..."
-		 python3 manage.py runserver 0.0.0.0:8000 --settings=dapi.settings.local
-	;;
-	2)
-		 echo "RUN AUTO LOCAL MIGRATIONS..."
-		 pip install -r requirements.txt
-		 python3 manage.py makemigrations --settings=dapi.settings.local
-		 python3 manage.py migrate --settings=dapi.settings.local
-	;;
-	3) 
-		 echo "CREATE SUPER USER..."
-		 python3 manage.py createsuperuser --settings=dapi.settings.local
-	;;
-	51) 
-		 echo "RUN WITH DEBUG CONSOLE (PROD)..."
-		 python3 manage.py runserver 0.0.0.0:8000 --settings=dapi.settings.prod
-	;;
-	52)
-		 echo "RUN AUTO PROD MIGRATIONS..."
-		 python3 manage.py makemigrations --settings=dapi.settings.prod
-		 python3 manage.py migrate --settings=dapi.settings.prod
-	;;
-	*)
-		 echo "Select a valid option 77"
-	;;
+  999) 
+     echo "Installing now ..."
+     pip install -r requirements.txt
+     python3 manage.py makemigrations --settings=dapi.settings.local
+     python3 manage.py migrate --settings=dapi.settings.local
+  ;;
+  0) 
+     echo "RUN WITH DEBUG CONSOLE (DEV)..."
+     python3 manage.py runserver 0.0.0.0:8000 --settings=dapi.settings.local
+  ;;
+  1)
+     echo "RUN AUTO LOCAL MIGRATIONS..."
+     pip install -r requirements/staging.txt
+     python3 manage.py makemigrations --settings=dapi.settings.local
+     python3 manage.py migrate --settings=dapi.settings.local
+     clear
+     echo "DONE!!!"
+  ;;
+  2) 
+     echo "CREATE SUPER USER..."
+     python3 manage.py createsuperuser --settings=dapi.settings.local
+  ;;
+  51) 
+     echo "RUN WITH DEBUG CONSOLE (PROD)..."
+     python3 manage.py runserver 0.0.0.0:8000 --settings=dapi.settings.prod
+  ;;
+  52)
+     echo "RUN AUTO PROD MIGRATIONS..."
+     python3 manage.py makemigrations --settings=dapi.settings.prod
+     python3 manage.py migrate --settings=dapi.settings.prod
+  ;;
+  *)
+     echo "Select a valid option 77"
+  ;;
 
-esacs
+esac  
